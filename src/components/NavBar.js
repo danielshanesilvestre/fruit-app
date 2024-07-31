cfcdc51aa08f87cdbbd016d26f4e92805102fa0e
Daniel <136662533+danielshanesilvestre@users.noreply.github.com>
2024-07-31 12:52:35 -0500

    Initial style for NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./NavBar.css";
 
 function NavBar() {
 
-  return <div>
+  return <nav>
     <NavLink to="/">Home</NavLink>
     <NavLink to="/fruits">Fruits</NavLink>
     <NavLink to="/fruits/new">Submit a new fruit</NavLink>
-  </div>
+  </nav>
 }
 
 
