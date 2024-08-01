2beef40923fcc1b12ae08f162c80a0251215b8c4
Daniel <136662533+danielshanesilvestre@users.noreply.github.com>
2024-08-01 17:01:20 -0500

    Render image

--- src/pages/FruitDetailsPage.js
+++ src/pages/FruitDetailsPage.js
@@ -6,10 +6,24 @@ import NavBar from "../components/NavBar";
 function FruitDetailsPage() {
   const { fruits } = useOutletContext();
   const { id } = useParams();
+  
+  if (fruits.length === 0) return <h1>Loading...</h1>;
+
   const fruit = fruits.find(fruit => fruit.id === id)
+
+  if (fruit === undefined) {
+    return <h1>Fruit not found!</h1>
+  }
+
   console.log(fruit);
+
   return <>
     <h1>{fruit.name}</h1>
+    <p>{fruit.description}</p>
+    <img src={fruit.image} style={{
+      width: "300px",
+      height: "300px",
+    }}/>
   </>
 }
 
