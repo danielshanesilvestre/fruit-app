d7a1f7e0613389d67d512044eb9232d562e05e38
Daniel <136662533+danielshanesilvestre@users.noreply.github.com>
2024-08-01 16:50:25 -0500

    Start single fruit page

--- src/pages/FruitDetailsPage.js
+++ src/pages/FruitDetailsPage.js
@@ -1,16 +1,16 @@
 import React from "react";
+import { useOutletContext, useParams } from "react-router-dom";
 import NavBar from "../components/NavBar";
 
 
-function FruitPage() {
+function FruitDetailsPage() {
+  const { fruits } = useOutletContext();
+  const { id } = useParams();
+  const fruit = fruits.find(fruit => fruit.id === id)
+  console.log(fruit);
   return <>
-    <header>
-      <NavBar />
-    </header>
-    <main>
-      This is placeholder text.
-    </main>
+    <h1>{fruit.name}</h1>
   </>
 }
 
-export default FruitPage;
+export default FruitDetailsPage;
